f22189671f2a6a9ea25c5574fb7ed82d835c92c8
Sergey <sergey@macbook.local>
2020-07-19 22:04:33 -0700

    Updates

---
+++ precache-manifest.5b68755510709c4ac26db72db40ab3bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bde8dfbcb09801918b01d2b6f92aa77f",
+    "url": "/react-counter/index.html"
+  },
+  {
+    "revision": "af2c0718f8db7ab0a5b7",
+    "url": "/react-counter/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "984ae9be82c3ad268427",
+    "url": "/react-counter/static/css/main.55d1fdf2.chunk.css"
+  },
+  {
+    "revision": "af2c0718f8db7ab0a5b7",
+    "url": "/react-counter/static/js/2.88522ff8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-counter/static/js/2.88522ff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "984ae9be82c3ad268427",
+    "url": "/react-counter/static/js/main.dd522ae2.chunk.js"
+  },
+  {
+    "revision": "0b3eb538c3c8bd942f22",
+    "url": "/react-counter/static/js/runtime-main.87a2a63b.js"
+  }
+]);
